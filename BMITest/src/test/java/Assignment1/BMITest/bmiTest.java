83b9198ac6769e812e1476b6aea43b0f0d3d0af9
Johan Landbris <jlasl09@student.lnu.se>
2015-11-20 18:05:06 +0100

    First test case.

--- BMITest/src/test/java/Assignment1/BMITest/bmiTest.java
+++ BMITest/src/test/java/Assignment1/BMITest/bmiTest.java
@@ -1,5 +1,29 @@
 package Assignment1.BMITest;
 
-public class bmiTest {
+import static org.junit.Assert.*;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class bmiTest 
+{
+	@Mock
+	Bmi testHelper;
+	
+	@Before
+	public void initMocks()
+	{
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void getsPositiveHeigthInt() throws Exception {
+		testHelper = new Bmi();
+	    Mockito.when(testHelper.getDouble()).thenReturn(-1);
+
+	    assertEquals(1, -1);
+	}
 }
