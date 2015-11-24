0f48db2e8da9c21fe0652995e342d40431bd243a
Johan Landbris <jlasl09@student.lnu.se>
2015-11-24 17:54:02 +0100

    REFACTOR. Cleaned up code. Uncovered code is either main or void.

--- BMITest/src/main/java/Assignment1/BMITest/Bmi.java
+++ BMITest/src/main/java/Assignment1/BMITest/Bmi.java
@@ -2,131 +2,98 @@ package Assignment1.BMITest;
 
 import java.util.Scanner;
 
-public class Bmi 
-{
+public class Bmi {
 	Scanner input = new Scanner(System.in);
-	public static void main(String[] args)
-	{
+
+	public static void main(String[] args) {
 		Bmi b = new Bmi();
 		b.printStartScreen();
 	}
-	public void printStartScreen()
-	{
+
+	public void printStartScreen() {
 		int height = 0;
 		int weight = 0;
-		System.out.println("Height in cm: ");	
+		System.out.println("Height in cm: ");
 		String inp = input.nextLine();
-		if (getInt(inp)==0)
-		{
+		if (getInt(inp) == 0) {
 			printError();
-		}
-		else
-		{
+		} else {
 			height = getInt(inp);
 			System.out.println("Weight in kg: ");
 			inp = input.nextLine();
 		}
-		if (getInt(inp)==0)
-		{
+		if (getInt(inp) == 0) {
 			printError();
-		}
-		else
-		{
-			
+		} else {
+
 			weight = getInt(inp);
-			double bmi = calculateBmi(height,weight);
-			System.out.println("Your BMI is: "+bmi+" and is defined as "+defineBmi(bmi)+".");
-		}	
+			double bmi = calculateBmi(height, weight);
+			System.out.println("Your BMI is: " + bmi + " and is defined as " + defineBmi(bmi) + ".");
+		}
 	}
-	
-	public void printError()
-	{
+
+	public void printError() {
 		System.out.println("Wrong input, you can only enter numbers between 0 and 250!");
 		System.exit(0);
 	}
-	public int getInt(String input)
-	{
-		if (isInteger(input)==true)
-		{
-		int inp = Integer.parseInt(input);
-		if (inp>0 && inp<250)
-		{
-			return inp;
-		}
-		else
-		{
-			return 0;
-		}
-		}
-		else
-		{
+
+	public int getInt(String input) {
+		if (isInteger(input) == true) {
+			int inp = Integer.parseInt(input);
+			if (inp > 0 && inp < 250) {
+				return inp;
+			} else {
+				return 0;
+			}
+		} else {
 			return 0;
 		}
 	}
-	
-	public static boolean isInteger(String str) 			
-	{
-	    if (str == null) 
-	    {
-	        return false;
-	    }
-	    int length = str.length();
-	    if (length == 0) 
-	    {
-	        return false;
-	    }
-	    int i = 0;
-	    if (str.charAt(0) == '-') 
-	    {
-	        if (length == 1) 
-	        {
-	            return false;
-	        }
-	        i = 1;
-	    }
-	    for (; i < length; i++) 
-	    {
-	        char c = str.charAt(i);
-	        if (c < '0' || c > '9') 
-	        {
-	            return false;
-	        }
-	    }
-	    return true;
+
+	public static boolean isInteger(String str) {
+		if (str == null) {
+			return false;
+		}
+		int length = str.length();
+		if (length == 0) {
+			return false;
+		}
+		int i = 0;
+		if (str.charAt(0) == '-') {
+			if (length == 1) {
+				return false;
+			}
+			i = 1;
+		}
+		for (; i < length; i++) {
+			char c = str.charAt(i);
+			if (c < '0' || c > '9') {
+				return false;
+			}
+		}
+		return true;
 	}
-	
-	public double calculateBmi(int height, int weight)
-	{
-		double heightInMeters = (double) height/100;
-		double bmi = (weight/(heightInMeters*heightInMeters));
-		bmi = bmi*10;
+
+	public double calculateBmi(int height, int weight) {
+		double heightInMeters = (double) height / 100;
+		double bmi = (weight / (heightInMeters * heightInMeters));
+		bmi = bmi * 10;
 		bmi = Math.round(bmi);
-		bmi = bmi /10;
+		bmi = bmi / 10;
 		return bmi;
 	}
-	
-	public String defineBmi(double bmi)
-	{
-		if (bmi>30)
-		{
+
+	public String defineBmi(double bmi) {
+		if (bmi > 30) {
 			return "Obese";
-		}
-		else if (bmi<=30&&bmi>25)
-		{
+		} else if (bmi <= 30 && bmi > 25) {
 			return "Overweight";
-		}
-		else if (bmi<=25&&bmi>19)
-		{
+		} else if (bmi <= 25 && bmi > 19) {
 			return "Normal";
-		}
-		else if (bmi<=19)
-		{
+		} else if (bmi <= 19) {
 			return "Underweigth";
-		}
-		else
-		{
+		} else {
 			return "";
 		}
 	}
 }
-
--- BMITest/src/test/java/Assignment1/BMITest/bmiTest.java
+++ BMITest/src/test/java/Assignment1/BMITest/bmiTest.java
@@ -8,137 +8,126 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
-public class bmiTest 
-{
+public class bmiTest {
 	@Mock
 	Bmi testHelper;
-	
+
 	@Before
-	public void initMocks()
-	{
+	public void initMocks() {
 		MockitoAnnotations.initMocks(this);
 	}
-	
+
 	@Test
-	public void getsPositiveIntTest(){
-	    Mockito.when(testHelper.getInt(null)).thenReturn(180);
-	    assertEquals(180, testHelper.getInt(null));
+	public void getsPositiveIntTest() {
+		Mockito.when(testHelper.getInt(null)).thenReturn(180);
+		assertEquals(180, testHelper.getInt(null));
 	}
-	
+
 	@Test
-	public void getRealisticIntTest()
-	{
-	    Mockito.when(testHelper.getInt(null)).thenReturn(180);		
-	    assertEquals(180, testHelper.getInt(null));
+	public void getRealisticIntTest() {
+		Mockito.when(testHelper.getInt(null)).thenReturn(180);
+		assertEquals(180, testHelper.getInt(null));
 	}
-	
+
 	@Test
-	public void getRealisticIntRevisitedTest()
-	{
+	public void getRealisticIntRevisitedTest() {
 		Bmi newHelper = new Bmi();
 		String input = "200";
 		int test = newHelper.getInt(input);
 		assertEquals(200, test);
 	}
+
 	@Test
-	public void returns0IfNotRealisticIntTest()
-	{
+	public void returns0IfNotRealisticIntTest() {
 		Bmi newHelper = new Bmi();
 		int test = newHelper.getInt("0");
-		assertEquals(0, test);	
+		assertEquals(0, test);
 	}
+
 	@Test
-	public void returns0IfInputIsNullTest()
-	{
+	public void returns0IfInputIsNullTest() {
 		Bmi newHelper = new Bmi();
 		int test = newHelper.getInt(null);
-		assertEquals(0, test);		
+		assertEquals(0, test);
 	}
-	
+
 	@Test
-	public void isInputIntegerTest()
-	{
+	public void isInputIntegerTest() {
 		String test = "100";
 		boolean bol = testHelper.isInteger(test);
 		assertTrue(bol);
 	}
-	
+
 	@Test
-	public void isInputIntegerLengthTest()
-	{
+	public void isInputIntegerLengthTest() {
 		String test = "";
 		boolean bol = testHelper.isInteger(test);
 		assertFalse(bol);
 	}
-	
+
 	@Test
-	public void calculateBmiTest()
-	{
-		Bmi calcHelper = new Bmi();					//Don't know why I needed to add a new Bmi. EDIT. Probably because of Mocking!
+	public void calculateBmiTest() {
+		Bmi calcHelper = new Bmi(); // Don't know why I needed to add a new Bmi.
+									// EDIT. Probably because of Mocking!
 		double test = 0;
 		int height = 200;
 		int weight = 100;
 		test = calcHelper.calculateBmi(height, weight);
-		assertEquals(25.0, test, 1);				//Allowing up to 1.0 wrong.
+		assertEquals(25.0, test, 1); // Allowing up to 1.0 wrong.
 	}
 
 	@Test
-	public void calculateBmiDifferentValuesTest()
-	{
+	public void calculateBmiDifferentValuesTest() {
 		Bmi calcHelper = new Bmi();
 		double test = 0;
 		int height = 190;
 		int weight = 80;
 		test = calcHelper.calculateBmi(height, weight);
-		assertEquals(22.16, test, 1);				
+		assertEquals(22.16, test, 1);
 	}
-	
+
 	@Test
-	public void isPersonObeseTest()
-	{
+	public void isPersonObeseTest() {
 		Bmi bmiHelper = new Bmi();
-		String test = bmiHelper.defineBmi(31);	
+		String test = bmiHelper.defineBmi(31);
 		assertEquals("Obese", test);
 	}
-	
+
 	@Test
-	public void isPersonOverweightTest()
-	{
+	public void isPersonOverweightTest() {
 		Bmi bmiHelper = new Bmi();
-		String test = bmiHelper.defineBmi(27);	
+		String test = bmiHelper.defineBmi(27);
 		assertEquals("Overweight", test);
 	}
-	
+
 	@Test
-	public void isPersonNormalWeightTest()
-	{
+	public void isPersonNormalWeightTest() {
 		Bmi bmiHelper = new Bmi();
-		String test = bmiHelper.defineBmi(25);	
+		String test = bmiHelper.defineBmi(25);
 		assertEquals("Normal", test);
 	}
-	
+
 	@Test
-	public void isPersonUnderWeightTest()
-	{
+	public void isPersonUnderWeightTest() {
 		Bmi bmiHelper = new Bmi();
-		String test = bmiHelper.defineBmi(19);	
+		String test = bmiHelper.defineBmi(19);
 		assertEquals("Underweigth", test);
 	}
+
 	@Test
-	public void displayStartScreenTest()
-	{
+	public void displayStartScreenTest() {
 		Mockito.doCallRealMethod().when(testHelper).printStartScreen();
-		//Mockito.verify(testHelper.printStartScreen());
+		// Mockito.verify(testHelper.printStartScreen());
 	}
+
 	@Test
-	public void bmiShouldReturnOnlyOneDecimalTest()
-	{
+	public void bmiShouldReturnOnlyOneDecimalTest() {
 		Bmi calcHelper = new Bmi();
 		double test = 0;
 		int height = 190;
 		int weight = 80;
 		test = calcHelper.calculateBmi(height, weight);
 		String decValue = Double.toString(test);
-		assertEquals("22.2", decValue);	
+		assertEquals("22.2", decValue);
 	}
 }
