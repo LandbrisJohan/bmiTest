76802bee25ae09de9192f7944f7e0859c8f1fb29
Johan Landbris <jlasl09@student.lnu.se>
2015-12-04 11:13:01 +0100

    GREEN. Implemented just enough code in order to succeed.

--- BMITest/src/main/java/Assignment1/BMITest/IsInteger.java
+++ BMITest/src/main/java/Assignment1/BMITest/IsInteger.java
@@ -3,6 +3,9 @@ package Assignment1.BMITest;
 public class IsInteger implements IsIntegerIF{
 
 	public boolean isInputInteger(String str) {
+		if (str == null) {
+			return false;
+		}
 		return true;
 	}
 }
